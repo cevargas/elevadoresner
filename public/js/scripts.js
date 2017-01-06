e473c10d634012c9b948b661b1ec851778814184
Carlos Vargas <cev@outlook.com>
2017-01-05 23:32:39 -0200

    Finalizacao Home

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,18 +7,67 @@ var APP = function () {
            captions: false
        });  
     };
+
+    mod.scrollNavbar = function(){
+      /**
+        * Listen to scroll to change header opacity class
+        */
+       function checkScroll(){
+           var startY = $('.fixed-nav-bar').height() * 2; //The point where the navbar changes in px
+
+           if($(window).scrollTop() > startY){
+               $('.fixed-nav-bar').addClass("scrolled");
+           }else{
+               $('.fixed-nav-bar').removeClass("scrolled");
+           }
+       }
+
+       if($('.fixed-nav-bar').length > 0){
+           $(window).on("scroll load resize", function(){
+               checkScroll();
+           });
+       }  
+    };
     
-    mod.responsive = function(){
-        $( window ).resize(function() {
-            $("#logo").removeClass('col-md-3').addClass('col-md-6');
+    mod.validateContact = function(){
+        $('#form-contato').validator(); 
+    };
+    
+    mod.validateOrcamento = function(){
+        $('#form-orcamento').validator(); 
+    };
+    
+    mod.scrollToTop = function(){
+        // hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	
+        $(window).scroll(function () {
+                if ($(this).scrollTop() > 100) {
+                        $('#back-top').fadeIn();
+                } else {
+                        $('#back-top').fadeOut();
+                }
         });
-        
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+                $('body,html').animate({
+                        scrollTop: 0
+                }, 800);
+                return false;
+        });
+	
     };
     
     return { 
         init: function(){
             mod.slider();
-            //mod.responsive();            
+            //mod.scrollNavbar();
+            mod.validateContact();   
+            mod.validateOrcamento();
+            mod.scrollToTop();
         }
     };
 
