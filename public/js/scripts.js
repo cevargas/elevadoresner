f20a362a5f95132160ec53ddd3232ef7d78ae8aa
Carlos Eduardo <Carlos Eduardo@pc-dudu>
2017-01-24 21:14:03 -0200

    Ajuste

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -98,7 +98,7 @@ var APP = function (PHOTOSWIPE) {
             mod.scrollToTop();
             mod.openMenuMobile();
             mod.outdated();
-            mod.resize();
+            mod.resize();       
             
             PHOTOSWIPE('.my-gallery');
         }
